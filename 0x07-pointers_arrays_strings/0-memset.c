f8a1615abbea5900bf69a40ea4cb9527e567e98c
Cebo59 <cebokuhledhlamini59@gmail.com>
2022-10-31 13:28:28 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,8 @@
 * to by @s with the constant byte @b
 * @n: bytes of the memory area pointed to by @s
 * @s: with the constant byte @b
-* @b: memory area pointer * Return: a pointer to the memory area @s
+* @b: memory area pointer
+* Return: a pointer to the memory area @s
 */
 char *_memset(char *s, char b, unsigned int n)
 {
