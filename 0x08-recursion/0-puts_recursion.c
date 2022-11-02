72b8b4a1bcce955b0631c86e5e16677fc1a371e4
Cebo59 <cebokuhledhlamini59@gmail.com>
2022-11-02 06:47:12 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,12 @@ Task 0
  */
 void _puts_recursion(char *s)
 {
-if (*s != '\0')
-{
-_putchar(*s);
-_puts_recursion(s + 1);
-}
-else
-_putchar('\n');
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
 
